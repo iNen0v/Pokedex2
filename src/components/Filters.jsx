f68655e085e56c57f1db59e42c2ab77812e7edd2
iNen0v <ivan.nenov89@gmail.com>
2024-12-24 16:04:43 +0200

    Filters и Home, Filters.scss, Home.scss

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import '../styles/Filters.scss';
 
 const pokemonTypes = [
  "normal", "fire", "water", "grass", "electric", "ice", 
@@ -6,33 +8,112 @@ const pokemonTypes = [
  "bug", "rock", "ghost", "dark", "dragon", "steel", "fairy"
 ];
 
+const typeColors = {
+ normal: '#A8A878',
+ fire: '#F08030',
+ water: '#6890F0',
+ grass: '#78C850',
+ electric: '#F8D030',
+ ice: '#98D8D8',
+ fighting: '#C03028',
+ poison: '#A040A0',
+ ground: '#E0C068',
+ flying: '#A890F0',
+ psychic: '#F85888',
+ bug: '#A8B820',
+ rock: '#B8A038',
+ ghost: '#705898',
+ dark: '#705848',
+ dragon: '#7038F8',
+ steel: '#B8B8D0',
+ fairy: '#EE99AC'
+};
+
 function Filters({ filters, setFilters }) {
+ const handleClearFilters = () => {
+   setFilters({
+     type: '',
+     minAttack: '',
+     minDefense: ''
+   });
+ };
+
  return (
-   <div className="advanced-filters">
-     <select 
-       value={filters.type} 
-       onChange={(e) => setFilters({...filters, type: e.target.value})}
+   <motion.div 
+     className="filters"
+     initial={{ opacity: 0, y: 20 }}
+     animate={{ opacity: 1, y: 0 }}
+     transition={{ duration: 0.3 }}
+   >
+     <div className="filters__group">
+       <label className="filters__label">Type</label>
+       <select 
+         value={filters.type} 
+         onChange={(e) => setFilters({...filters, type: e.target.value})}
+         className="filters__select"
+       >
+         <option value="">All types</option>
+         {pokemonTypes.map(type => (
+           <option 
+             key={type} 
+             value={type}
+             style={{background: typeColors[type]}}
+           >
+             {type.charAt(0).toUpperCase() + type.slice(1)}
+           </option>
+         ))}
+       </select>
+     </div>
+
+     <div className="filters__group">
+       <label className="filters__label">Min Attack</label>
+       <div className="filters__input-wrapper">
+         <input
+           type="range"
+           min="0"
+           max="150"
+           value={filters.minAttack || 0}
+           onChange={(e) => setFilters({...filters, minAttack: e.target.value})}
+           className="filters__range"
+         />
+         <input
+           type="number"
+           placeholder="0"
+           value={filters.minAttack}
+           onChange={(e) => setFilters({...filters, minAttack: e.target.value})}
+           className="filters__number"
+         />
+       </div>
+     </div>
+
+     <div className="filters__group">
+       <label className="filters__label">Min Defense</label>
+       <div className="filters__input-wrapper">
+         <input
+           type="range"
+           min="0"
+           max="150"
+           value={filters.minDefense || 0}
+           onChange={(e) => setFilters({...filters, minDefense: e.target.value})}
+           className="filters__range"
+         />
+         <input
+           type="number"
+           placeholder="0"
+           value={filters.minDefense}
+           onChange={(e) => setFilters({...filters, minDefense: e.target.value})}
+           className="filters__number"
+         />
+       </div>
+     </div>
+
+     <button 
+       onClick={handleClearFilters}
+       className="filters__clear"
      >
-       <option value="">All types</option>
-       {pokemonTypes.map(type => (
-         <option key={type} value={type}>{type}</option>
-       ))}
-     </select>
-
-     <input
-       type="number"
-       placeholder="Min Attack"
-       value={filters.minAttack}
-       onChange={(e) => setFilters({...filters, minAttack: e.target.value})}
-     />
-
-     <input
-       type="number"
-       placeholder="Min Defense"
-       value={filters.minDefense}
-       onChange={(e) => setFilters({...filters, minDefense: e.target.value})}
-     />
-   </div>
+       Clear Filters
+     </button>
+   </motion.div>
  );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,98 +4,114 @@ import { useDispatch, useSelector } from 'react-redux';
 import PokemonList from '../components/PokemonList';
 import Filters from '../components/Filters';
 import { fetchPokemons } from '../redux/actions';
+import '../styles/Home.scss';
 
 function Home() {
-  const dispatch = useDispatch();
-  const { data: pokemons, loading } = useSelector(state => state.pokemons);
-  const [sortBy, setSortBy] = useState('id');
-  const [filters, setFilters] = useState({
-    type: '',
-    minAttack: '',
-    minDefense: ''
-  });
+ const dispatch = useDispatch();
+ const { data: pokemons, loading } = useSelector(state => state.pokemons);
+ const [sortBy, setSortBy] = useState('id');
+ const [filters, setFilters] = useState({
+   type: '',
+   minAttack: '',
+   minDefense: ''
+ });
 
-  useEffect(() => {
-    dispatch(fetchPokemons());
-  }, [dispatch]);
+ useEffect(() => {
+   dispatch(fetchPokemons());
+ }, [dispatch]);
 
-  const filterAndSortPokemons = () => {
-    let filtered = pokemons;
+ const filterAndSortPokemons = () => {
+   let filtered = pokemons;
 
-    if (filters.type) {
-      filtered = filtered.filter(pokemon =>
-        pokemon.types.some(t => t.type.name === filters.type)
-      );
-    }
+   if (filters.type) {
+     filtered = filtered.filter(pokemon =>
+       pokemon.types.some(t => t.type.name === filters.type)
+     );
+   }
 
-    if (filters.minAttack) {
-      filtered = filtered.filter(pokemon =>
-        pokemon.stats[1].base_stat >= parseInt(filters.minAttack)
-      );
-    }
+   if (filters.minAttack) {
+     filtered = filtered.filter(pokemon =>
+       pokemon.stats[1].base_stat >= parseInt(filters.minAttack)
+     );
+   }
 
-    if (filters.minDefense) {
-      filtered = filtered.filter(pokemon =>
-        pokemon.stats[2].base_stat >= parseInt(filters.minDefense)
-      );
-    }
+   if (filters.minDefense) {
+     filtered = filtered.filter(pokemon =>
+       pokemon.stats[2].base_stat >= parseInt(filters.minDefense)
+     );
+   }
 
-    return [...filtered].sort((a, b) => {
-      switch(sortBy) {
-        case 'name':
-          return a.name.localeCompare(b.name);
-        case 'attack':
-          return b.stats[1].base_stat - a.stats[1].base_stat;
-        case 'defense':
-          return b.stats[2].base_stat - a.stats[2].base_stat;
-        default:
-          return a.id - b.id;
-      }
-    });
-  };
+   return [...filtered].sort((a, b) => {
+     switch(sortBy) {
+       case 'name':
+         return a.name.localeCompare(b.name);
+       case 'attack':
+         return b.stats[1].base_stat - a.stats[1].base_stat;
+       case 'defense':
+         return b.stats[2].base_stat - a.stats[2].base_stat;
+       default:
+         return a.id - b.id;
+     }
+   });
+ };
 
-  const filteredAndSortedPokemons = filterAndSortPokemons();
+ const filteredAndSortedPokemons = filterAndSortPokemons();
 
-  return (
-    <div className="min-h-screen bg-gray-900 text-white">
-      <div className="w-full max-w-[300px] mx-auto py-6">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"
-          alt="Pokemon Logo"
-          className="w-full"
-        />
-      </div>
+ return (
+   <div className="pokemon-home">
+     <div className="pokemon-home__background-pattern" />
+     <div className="pokemon-home__decorative-line" />
 
-      <div className="container mx-auto px-4 py-8">
-        <div className="mb-8">
-          <div className="flex gap-4">
-            <select
-              value={sortBy}
-              onChange={(e) => setSortBy(e.target.value)}
-              className="p-2 rounded bg-gray-800 text-white"
-            >
-              <option value="id">Number</option>
-              <option value="name">Name</option>
-              <option value="attack">Attack</option>
-              <option value="defense">Defense</option>
-            </select>
+     <motion.div 
+       className="pokemon-home__logo-container"
+       initial={{ y: -20, opacity: 0 }}
+       animate={{ y: 0, opacity: 1 }}
+       transition={{ duration: 0.5 }}
+     >
+       <img
+         src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg"
+         alt="Pokemon Logo"
+         className="pokemon-home__logo"
+       />
+     </motion.div>
 
-            <Filters filters={filters} setFilters={setFilters} />
-          </div>
-        </div>
+     <div className="pokemon-home__content">
+       <div className="pokemon-home__control-panel">
+         <div className="pokemon-home__controls">
+           <select
+             value={sortBy}
+             onChange={(e) => setSortBy(e.target.value)}
+             className="pokemon-home__select"
+           >
+             <option value="id">Number</option>
+             <option value="name">Name</option>
+             <option value="attack">Attack</option>
+             <option value="defense">Defense</option>
+           </select>
 
-        {loading ? (
-          <div className="text-center">Loading...</div>
-        ) : (
-          <PokemonList
-            pokemons={filteredAndSortedPokemons}
-            onPokemonSelect={() => {}}
-            battleMode={false}
-          />
-        )}
-      </div>
-    </div>
-  );
+           <Filters filters={filters} setFilters={setFilters} />
+         </div>
+       </div>
+
+       {loading ? (
+         <motion.div 
+           className="pokemon-home__loading"
+           animate={{ opacity: [0.5, 1, 0.5] }}
+           transition={{ duration: 1.5, repeat: Infinity }}
+         >
+           <div className="pokemon-home__loading-text">Loading Pokémon...</div>
+           <div className="pokemon-home__loading-spinner" />
+         </motion.div>
+       ) : (
+         <PokemonList
+           pokemons={filteredAndSortedPokemons}
+           onPokemonSelect={() => {}}
+           battleMode={false}
+         />
+       )}
+     </div>
+   </div>
+ );
 }
 
 export default Home;
