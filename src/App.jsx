3ba7056065d3602d871b3d8297cd777025a419f8
iNen0v <ivan.nenov89@gmail.com>
2025-02-12 16:44:20 +0200

    Update App component and add Vite config

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,13 @@ import BattleArena from './pages/BattleArena';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-export const BASE_PATH = '/pokedex2-six-psivercel.app';
-
 function App() {
   return (
     <Provider store={store}>
-      <Router basename={BASE_PATH}> 
+      <Router basename="/pokedex2-six-psivercel.app">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/pokemon/:id" element={<PokemonDetails />} /> 
+          <Route path="/pokemon/:id" element={<PokemonDetails />} />
           <Route path="/battle" element={<BattlePage />} />
           <Route path="/battle-arena/:pokemon1Id/:pokemon2Id" element={<BattleArena />} />
         </Routes>
